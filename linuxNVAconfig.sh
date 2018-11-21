9fb1c816ccdd61e6e1b6cdf957791b6c2793c605
Jose Moreno <jomore@microsoft.com>
2018-11-21 22:16:23 +0100

    typo fixed in public IP output variable

--- linuxNVAconfig.sh
+++ linuxNVAconfig.sh
@@ -7,8 +7,10 @@ sudo -i sysctl -w net.ipv4.ip_forward=1
 # Install Quagga
 sudo apt-get install quagga -y
 sudo touch /etc/quagga/zebra.conf
+sudo touch /etc/quagga/bgpd.conf
+
 sudo chown quagga.quagga /etc/quagga/zebra.conf
-sudo chmod 640 /etc/quagga/zebra.conf
 sudo echo "password $1" >> /etc/quagga/zebra.conf
+sudo chmod 640 /etc/quagga/zebra.conf
 sudo systemctl enable quagga
 sudo systemctl restart quagga
